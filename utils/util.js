05a191fc18ea4650586851d39699bcb7ae669487
Tajx <1135854336@qq.com>
2021-10-20 16:58:35 +0800

    完成AI算法设计

--- utils/util.js
+++ utils/util.js
@@ -67,6 +67,86 @@ function observe(obj, key, watchFun, deep, page) {
   })
 }
 
+//抉择函数，选择最优的操作
+//对手手牌状态，己方手牌状态，放置区手牌状态, 放置区牌顶
+//顺序CDSH
+//返回一个值0 摸牌， 1，2，3，4分别表示CDSH
+function Mcts(enemyCnt, selfCnt, placeArea, palceTop_card) {
+  let placeAreaCnt = [0,0,0,0];
+  for (var i = 0; i < 4; ++i) {
+    placeAreaCnt[i] = placeArea[i].length;
+  }
+  //统计牌库的状态
+  let leftCard = [13,13,13,13];
+  for (var i = 0; i < 4; ++i) {
+    leftCard[i] -= enemyCnt[i];
+    leftCard[i] -= selfCnt[i];
+    leftCard[i] -= placeAreaCnt[i];
+  }
+  var sumSelf = 0, sumLeft = 0;
+  for (var i = 0; i < 4; ++i) {
+    sumSelf += selfCnt[i];
+    sumLeft += leftCard[i];
+  }
+  //如果没有手牌，那么只能摸牌
+  if (sumLeft == 0) {
+    return 0;
+  }
+  //放置区顶牌对应的下标
+  var index = -1;
+  switch(palceTop_card[0]) {
+    case 'C':
+      index = 0;
+      break;
+    case 'D':
+      index = 1;
+      break;
+    case 'S':
+      index = 2;
+      break;
+    case 'H':
+      index = 3;
+      break;
+  }
+  //如果牌库剩余牌不大于手牌，则全部操作为出牌
+  if (sumSelf >= sumLeft) {
+    //获得牌堆中与当前放置区不同的牌的数量
+    //取最大值，这样对手摸牌的话，吃牌概率最大
+    var maxCardCnt = -1, maxIndex = -1;
+    for (var i = 0; i < 4; ++i) {
+        if (leftCard[i] >= maxCardCnt && i != index && selfCnt[i] != 0) {
+          maxCardCnt = leftCard[i];
+          maxIndex = i;
+        }
+    }
+    //出的牌为对应的下标
+    return maxIndex + 1;
+  }
+  //牌库的牌数量多于手牌，考虑用蒙特卡洛树搜索进行决策
+  //对剩余牌库进行随机化排序，使得摸牌操作的后继状态唯一
+  var cardList = [];
+  for (var i = 0; i < 4; ++i) {
+    var tmp = leftCard[i];
+    while(tmp) {
+      tmp--;
+      cardList.append(i);
+    }
+  }
+  cardList.sort(function(){
+    return Math.random - 0.5;
+  });
+  //进行抉择
+  var possilbeSelect = [0];
+  for (var i = 0; i < 4; ++i) {
+    if (selfCnt[i] != 0)
+      possilbeSelect.append(i+1);
+  }
+  possilbeSelect.sort(function(){
+    return Math.random - 0.5;
+  });
+  return possilbeSelect[0];
+}
+
 module.exports = {
   json2Form:json2Form,
   formatTime,
